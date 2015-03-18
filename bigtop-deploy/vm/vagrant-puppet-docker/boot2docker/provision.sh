b18ae0e8654239c7f11b81d66082b9eed627e2d1
fonhorst <alipoov@gmail.com>
2015-03-18 19:07:31 +0300

    correct shell name for boot2docker shell provision script

--- bigtop-deploy/vm/vagrant-puppet-docker/boot2docker/provision.sh
+++ bigtop-deploy/vm/vagrant-puppet-docker/boot2docker/provision.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 echo "export HTTP_PROXY=http://proxy.ifmo.ru:3128" >> /var/lib/boot2docker/profile
 echo "export http_proxy=http://proxy.ifmo.ru:3128" >> /var/lib/boot2docker/profile
