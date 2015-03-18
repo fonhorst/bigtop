d8c11df4c1144ea712b4a4e82f1062d0cdbc4763
fonhorst <alipoov.nb@gmail.com>
2015-03-18 16:32:15 +0300

    corrected images

--- bigtop-deploy/vm/vagrant-puppet-docker/spark_image/hiera_settings.sh
+++ bigtop-deploy/vm/vagrant-puppet-docker/spark_image/hiera_settings.sh
@@ -14,6 +14,6 @@ mkdir -p /etc/puppet/hieradata
 cp /bigtop-home/bigtop-deploy/puppet/hiera.yaml /etc/puppet
 cp -r /bigtop-home/bigtop-deploy/puppet/hieradata/bigtop/ /etc/puppet/hieradata/
 cat > /etc/puppet/hieradata/site.yaml << EOF
-Bigtop::bigtop_yumrepo_uri: "http://bigtop01.cloudera.org:8080/view/Releases/job/Bigtop-0.8.0/label=centos6/6/artifact/output/"
+Bigtop::bigtop_yumrepo_uri: http://bigtop01.cloudera.org:8080/view/Releases/job/Bigtop-0.8.0/label=centos6/6/artifact/output/
 bigtop::jdk_package_name: $jdk
 EOF
