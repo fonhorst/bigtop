014a56c669a55146dbea0de60dab73ad6151ad55
fonhorst <alipoov@gmail.com>
2015-03-18 18:59:52 +0300

    boot2docker adapted for proxy

---
+++ bigtop-deploy/vm/vagrant-puppet-docker/boot2docker/provision.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "export HTTP_PROXY=http://proxy.ifmo.ru:3128" >> /var/lib/boot2docker/profile
+echo "export http_proxy=http://proxy.ifmo.ru:3128" >> /var/lib/boot2docker/profile
+/etc/init.d/docker restart
